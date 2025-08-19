5d529ca247778f7b2ae5010bd98f9c018e8efce0
NotTete <55021970+NotTete@users.noreply.github.com>
2025-08-19 23:04:38 +0100

    added new hover options and more menus

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,3 +1,4 @@
 export const discordLink = "https://discord.gg/8TAu3eGWa8";
 export const githubLink = "https://github.com/UCppM";
 export const instagramLink = "https://www.instagram.com/ucppm_/";
+export const ucmLink = "https://www.ucm.es";
