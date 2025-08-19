8d7a39949e8798389167fe1ac848e6084fc25e1a
NotTete <55021970+NotTete@users.noreply.github.com>
2025-08-19 11:57:06 +0100

    test

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,3 +1,3 @@
-export const discordLink = "https://github.com/UCppM";
+export const discordLink = "https://discord.gg/8TAu3eGWa8";
 export const githubLink = "https://github.com/UCppM";
 export const instagramLink = "https://www.instagram.com/ucppm_/";
