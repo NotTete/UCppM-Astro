6289c947bac79e3069eb2ef58915ce6624af2ca5
NotTete <55021970+NotTete@users.noreply.github.com>
2025-08-20 16:27:47 +0100

    added bento grid

---
+++ tailwind.config.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+  content: ['./src/**/*.{astro,js,ts,jsx,tsx}'],
+};
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
-  ],
-  theme: {
-    extend: {},
-  },
-};
