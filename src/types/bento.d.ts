04f77c721661505ca1197370685958be180f396d
NotTete <55021970+NotTete@users.noreply.github.com>
2025-08-27 13:58:03 +0100

    added responsive

---
+++ src/types/bento.d.ts
@@ -0,0 +1,17 @@
+export type BentoLayout = [number, number, number, number];
+
+export type BentoLayouts = {
+    sm?: BentoLayout;
+    md?: BentoLayout;
+    lg?: BentoLayout;
+    default?: BentoLayout;
+}
+
+export type BentoMatrix = [number, number];
+
+export type BentoMatrices = {
+    sm?: BentoMatrix;
+    md?: BentoMatrix;
+    lg?: BentoMatrix;
+    default?: BentoMatrix;
+}
