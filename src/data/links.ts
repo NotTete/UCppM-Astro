a7f40ee014af17868d97ffecf446ee9b2a9683b7
NotTete <55021970+NotTete@users.noreply.github.com>
2025-08-28 11:04:34 +0100

    added footer and improved responsiveness

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,4 +1,11 @@
 export const discordLink = "https://discord.gg/8TAu3eGWa8";
 export const githubLink = "https://github.com/UCppM";
 export const instagramLink = "https://www.instagram.com/ucppm_/";
+export const youtubeLink = "https://www.youtube.com/@UCppM";
+
 export const ucmLink = "https://www.ucm.es";
+
+export const ucppmEmail = "uccpm@ucm.es";
+
+export const sociosLink = "https://docs.google.com/forms/d/e/1FAIpQLScxhUCcndJK4XCLhBpiYu1Ru-n93opxCvYIyAw4ey3_Ekqptw/viewform";
+export const estatutosLink = "https://docs.google.com/document/d/e/2PACX-1vROHkrB9b5j7XrWiFbIN7JIX4BIsuu3VArcAZMCu-ZMj4DPBuSKl6vuctlc-Ka_-bF_FxKWDhL6gIuv/pub";
